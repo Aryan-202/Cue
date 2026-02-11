2e91500f0a03d77b6085d7beee20c55a34d76d4f
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2026-02-12 01:44:39 +0530

    feat: implement dependencies for the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,4 +53,10 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    implementation(libs.androidx.media3.exoplayer)
+    implementation(libs.androidx.media3.ui)
+    implementation(libs.coil.compose)
+    implementation(libs.accompanist.permissions)
+
 }
