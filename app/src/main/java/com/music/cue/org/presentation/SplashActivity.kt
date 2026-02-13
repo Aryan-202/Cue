d2084f8df783fe08f42e60c0252eded8ffd9f39c
Aryan Vishwakarma <aryanvishwakarma275@gmail.com>
2026-02-13 17:24:35 +0530

    feat: implement splash screen with navigation
    Adds the splash screen for the application and sets it as the initial launch activity.
    Implements the "Get Started" button functionality to navigate to the MainActivity.
    Updates the background drawable with a new vector graphic.
    Refactors SplashActivity to use Jetpack Compose.

--- app/src/main/java/com/music/cue/org/presentation/SplashActivity.kt
+++ app/src/main/java/com/music/cue/org/presentation/SplashActivity.kt
@@ -1,14 +1,21 @@
 package com.music.cue.org.presentation
 
+import android.content.Intent
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
-import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
-import com.music.cue.org.R
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import com.music.cue.org.ui.theme.SplashScreen
 
-class SplashActivity : AppCompatActivity() {
+class SplashActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContent {
+            SplashScreen (
+                onStartClick ={
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+            )
+        }
     }
 }
